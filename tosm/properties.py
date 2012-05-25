c19d9fcbfbee34257ed9987cd3c2486c546f01de
alvesjnr <alvesjunior.antonio@gmail.com>
2012-05-25 13:34:19 +0200

    Basic functions of _BaseProperty working

--- tosm/properties.py
+++ tosm/properties.py
@@ -1,10 +1,26 @@
 
-class _BaseProperty:
+# from exceptions import AbstractClassError, NotImplementedMethodError, KeylessArgError, NotAllowedArgument
 
-    def __init__(self):
-        pass
 
-    def _implicid_validation(self):
+class _BaseProperty(object):
+    
+    _allowed_args = ["doc","validation"]
+
+    def __init__(self, default=None, *args, **kwargs):
+        
+        if args:
+            raise KeylessArgError()
+        
+        for key,value in kwargs:
+            if key in _allowed_args:
+                setattr(self, "_key_"+key, value)
+            else:
+                raise NotAllowedArgument()
+
+        if default:
+            self._set_value(default)
+
+    def _implicid_validation(self, value):
         """
             The implicid validation uses general keys to valid the property
             Examples of keys are: max, min, validation_function, etc.
@@ -12,9 +28,33 @@ class _BaseProperty:
             API reference for valid keys for each property
         """
 
-    def _validate(self):
-        raise NotImplementedMethodError()
+    def _validate(self, value):
+        """
+            The validate function is a wrapper to the externally provided
+            validation function
+        """
+        if hasattr(self,"validation"):
+            self.validation(value)
 
+    def __get__(self, instance, cls):
+        return getattr(instance, self.attr_name)
+
+    def __set__(self, instance, value):
+        for attr_name, attr_value in instance.__class__.__dict__.items():
+            if attr_value == self:
+                self.attr_name = '_attributename_'+attr_name
+                setattr(instance, self.attr_name, value)
+                break
+
+    def _set_value(self, value):
+        """
+            Set the value inside the property.
+            This function is internally called by the __set__ method after
+            a validation.
+        """
+        self._implicid_validation(value)
+        self._validate(value)
+        setattr(self, self.attr_name, value)
 
 class StringProperty(_BaseProperty):
 
@@ -25,10 +65,19 @@ class StringProperty(_BaseProperty):
 class IntegerProperty(_BaseProperty):
 
     def __init__(self,**kwargs):
-        pass
+        pass    
 
 
 class PositiveIntegerProperty(IntegerProperty):
 
     def __init__(self,**kwargs):
         pass
+
+if __name__=='__main__':
+    class A(object):
+        a = _BaseProperty()
+
+    a = A()
+    a.a = 10
+    import pdb; pdb.set_trace()
+    print a.a
