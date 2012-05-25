197625bba73eede175de44224ed05abb409a7806
alvesjnr <alvesjunior.antonio@gmail.com>
2012-05-25 15:51:35 +0200

    README.md

--- tosm/tests/test_properties.py
+++ tosm/tests/test_properties.py
@@ -1,15 +1,28 @@
 
 import unittest
-from properties import *
+from properties import _BaseProperty
 
 
-class TestBaseProperty(unittest.Test):
+class TestBaseProperty(unittest.TestCase):
 
     def test_a_base_property_basic_consistence(self):
         A = self.get_class_a()
         a = A()
         a.a = 10
-        self.AssertTrue(isisntace(a.a, _BaseProperty))
+        self.assertTrue(isinstance(a.a, _BaseProperty))
+
+    def test_b_default_value(self):
+        B = get_class_with_default()
+        b = B()
+        self.assertTrue(b.b == 10)
+
+    def test_c1_validator(self):
+        pass
+
+    def test_d_doc(self):
+        D = get_class_with_doc()
+        d = D()
+        self.assertTrue(d.d.doc == "Yes, this is doc!") #FIXME!!!
     
     @staticmethod
     def get_class_a():
@@ -38,3 +51,10 @@ class TestBaseProperty(unittest.Test):
             d = _BaseProperty(doc="Yes, this is doc!")
 
         return D
+
+class TestIntegerProperty(unittest.TestCase):
+
+    pass
+
+if __name__ == '__main__':
+    unittest.main()
