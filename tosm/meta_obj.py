418babcfdc1884b5864631928c16c8b8efb68e97
Antonio Ribeiro <alvesjunior.antonio@gmail.com>
2012-05-29 21:11:16 +0200

    cleaning branch

--- tosm/meta_obj.py
+++ tosm/meta_obj.py
@@ -13,9 +13,7 @@ class TobjMetaclass(type):
             if isinstance(value, _BaseProperty):
                 prop[key] = (value.__class__, value._property_index)
                 consecutive.append((value._property_index,key))
-        # blah = dct.items()[0][1]
-        # import pdb; pdb.set_trace()
-        # isinstance(blah,_BaseProperty)
+
         consecutive.sort(key=lambda a : a[0])
         dct.update({'_tosm_properties': prop,
                     '_consecutive_arguments': [name for i,name in consecutive],
